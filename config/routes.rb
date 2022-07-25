af401395f90900f5691f4c1a1c0d0c059f0f1353
mattand <mjande@umich.edu>
2022-07-25 14:59:15 -0400

    Add styles to user actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   end
     
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    confirmations: 'users/confirmations',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
