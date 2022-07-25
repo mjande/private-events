2e64faee18a00054bd650d25a73731980f930971
mattand <mjande@umich.edu>
2022-07-25 08:55:34 -0400

    Add files to configure devise turbo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :events
+
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+    
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
