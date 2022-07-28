a844799d751fbda5c73acfcf7361585931210608
mattand <mjande@umich.edu>
2022-07-28 13:18:33 -0400

    Separate events on root_path

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :invites, foreign_key: "attended_event_id"
   has_many :attendees, through: :invites, source: :users, foreign_key: "attendee_id"
+
+  scope :past, -> { where("date < ?", Date.today) }
+  scope :upcoming, -> { where("date > ?", Date.today) }
 end
